b88feeda2b7ba5be2b29518a0977f00bd106e828
vigan <vigan.abd@gmail.com>
2019-09-26 21:09:08 +0200

    decimal number utilities added

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 'use strict'
 
+const number = require('./src/number')
 const bn = require('./src/bn')
 const vwap = require('./src/vwap')
 const stdDeviation = require('./src/std-deviation')
 
 module.exports = {
+  ...number,
   ...bn,
   ...vwap,
   ...stdDeviation
--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,12 @@
 'use strict'
 
+const number = require('./number.test')
 const bn = require('./bn.test')
 const vwap = require('./vwap.test')
 const stdDeviation = require('./std-deviation.test')
 
 describe('*** Unit testing! ***', () => {
+  number()
   bn()
   vwap()
   stdDeviation()
