8db2f9252c0a8d409e7a6e4ac16250cdbb8b8fea
vigan <vigan.abd@gmail.com>
2020-11-20 10:57:08 +0100

    updates examples

--- examples/array.js
+++ examples/array.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { avg, max, min, nBN } = require('..')
+const { avg, max, min, median, filterMedian, nBN } = require('..')
 
 const primiteveArray = [13, 15, 11, 12, 14]
 
@@ -28,3 +28,17 @@ const primitiveMax = max(primiteveArray)
 console.log(primitiveMax) // prints 15
 const complexMax = max(complexArray, (a) => a.price)
 console.log(complexMax) // 10724
+
+const primitiveMedian = median(primiteveArray)
+console.log(primitiveMedian.toString()) // prints 13
+
+const filtered = filterMedian([
+  { price: '10261.235', volume: '0.33' },
+  { price: '10224.2567', volume: '2.13' },
+  { price: '10724', volume: '5.26' },
+  { price: '10161', volume: '1' },
+  { price: '10232', volume: '0.77' },
+  { price: '10210.25847', volume: '1.52' },
+  { price: '10261.826', volume: '13.22' }
+], '0.005', (x) => x.price)
+console.log(filtered) // filters out -> { price: '10724', volume: '5.26' }
