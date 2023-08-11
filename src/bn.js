183e9430f0e9609c0d7cef36c0511a1bbcd86387
batrudinych <denis.fatkhudinov@gmail.com>
2023-08-11 15:37:42 -0400

    check for nan

--- src/bn.js
+++ src/bn.js
@@ -25,9 +25,14 @@ const nBN = (value) => {
 
 /**
  * @param {string|number|BigNumber} value
+ * @param {object} [opts]
+ * @param {boolean} [opts.allowNegative=true]
+ * @param {boolean} [opts.allowZero=true]
+ * @param {boolean} [opts.allowDecimals=true]
  */
 const validateBN = (value, opts = { allowNegative: true, allowZero: true, allowDecimals: true }) => {
   const n = nBN(value)
+  if (n.isNaN()) throw new Error('ERR_NOT_NUM')
   if (opts.allowZero === false && n.isZero()) throw new Error('ERR_NUM_ZERO')
   if (opts.allowNegative === false && n.isNegative()) throw new Error('ERR_NUM_NOT_POS')
   if (opts.allowDecimals === false && !n.isInteger()) throw new Error('ERR_NUM_WITH_PRECISION')
